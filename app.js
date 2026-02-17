2bc0f903d25d3c8458c9525cdcee1f532e9b3173
masatomoota <129290880+masatomoota@users.noreply.github.com>
2026-02-17 13:33:22 +0900

    feat: add time offset setting and rename app to Aerial_masatomo

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const fs = require('fs');
 const path = require("path");
 const AutoLaunch = require('auto-launch');
 let autoLauncher = new AutoLaunch({
-    name: 'Aerial',
+    name: 'Aerial_masatomo',
 });
 const SunCalc = require('suncalc');
 
@@ -463,6 +463,7 @@ function setUpConfigFile() {
     }
     //time & location settings
     store.set('timeOfDay', store.get('timeOfDay') ?? false);
+    store.set('timeOffsetMinutes', Number(store.get('timeOffsetMinutes')) || 0);
     store.set('sunrise', store.get('sunrise') ?? "06:00");
     store.set('sunset', store.get('sunset') ?? "18:00");
     store.set('useLocationForSunrise', store.get('useLocationForSunrise') ?? false);
